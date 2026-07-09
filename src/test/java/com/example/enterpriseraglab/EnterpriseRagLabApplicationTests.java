033cac712360b9fe005e8283379d762ac9542242
tommyJ874 <119998380+tommyJ874@users.noreply.github.com>
2026-07-09 21:28:36 +0900

    Initial enterprise RAG lab project

---
+++ src/test/java/com/example/enterpriseraglab/EnterpriseRagLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enterpriseraglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseRagLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
